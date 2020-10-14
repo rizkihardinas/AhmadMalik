d13e5a896bf9e6e78094737ad7781ad5df75dee3
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2020-10-14 11:32:48 +0700

    Update Merchants.php

--- application/controllers/Merchants.php
+++ application/controllers/Merchants.php
@@ -87,7 +87,56 @@ class Merchants extends CI_Controller
 		
 		redirect('merchants');
 	}
+	function update(){
+		$name = $this->input->post('name');
+		$id = $this->input->post('id');
+		$address  = $this->input->post('address');
+		$min_price = $this->input->post('min_price');
+		$address = $this->input->post('address');
+		$max_price = $this->input->post('max_price');
+		$description = $this->input->post('description');
+		$latLng = $this->input->post('latitude');
 
+		$latLng = str_replace('LngLat(', '', $latLng);
+		$latLng = str_replace(')', '', $latLng);
+		$dataLatLng = explode(',', $latLng);
+		$latitude = $dataLatLng[0];
+		$longitude = $dataLatLng[1];
+		
+		
+		
+        if ($this->input->post('customFile')) {
+        	$config['upload_path']          = './uploads/';
+	        $config['allowed_types']        = 'gif|jpg|png';
+	        // $config['max_size']             = 100;
+	        // $config['max_width']            = 1024;
+	        // $config['max_height']           = 768;
+	        $this->load->library('upload', $config);
+
+	        if ( ! $this->upload->do_upload('customFile'))
+	        {
+	                $this->session->set_flashdata('msg',$this->upload->display_errors());
+					$this->session->set_flashdata('response','warning');
+	        }
+	        else
+	        {
+	                $file = $this->upload->data();
+	                $data['photo'] = $file['file_name'];
+	        }
+        }
+        $data = array(
+			'name' => $name,
+			'min_price' => $min_price,
+			'max_price' => $max_price,
+			'description' => $description,
+			'latitude' => $latitude,
+			'longitude' => $longitude
+		);
+       	$this->db_model->update('merchants',$data,array('id'=>$id));
+		$this->session->set_flashdata('msg','Update Berhasil!');
+		$this->session->set_flashdata('response','success');
+		redirect('merchants');
+	}
 	function dt_merchants(){
 
           // Datatables Variables
