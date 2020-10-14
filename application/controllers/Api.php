cf42a3ba4c9a75c3e88f22305b8cf7cf2049bb4d
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2020-10-14 16:39:02 +0700

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -26,13 +26,32 @@ class Api extends CI_Controller
 		$store = $this->api->getStore($data);
 		echo json_encode($store);
 	}
+	function getUlasan(){
+		$id = $this->input->post('id');
+		$data = $this->api->getUlasan($id);
+		echo json_encode($data);
+	}
 	function insertUlasan(){
 		$idUser = $this->input->post('idUser');
 		$idMerchant = $this->input->post('idMerchant');
 		$rating = $this->input->post('rating');
-		$rating = $this->input->post('rating');
+		$review = $this->input->post('review');
 
-
+		$check = $this->api->countDetailData("rating",$data);
+		if ($check > 0) {
+			$array = array('code' => 400);
+			echo json_encode($array);
+		}else{
+			$dataInsert = array(
+				'idUser' =>$idUser,
+				'idMerchant' =>$idMerchant,
+				'rating' =>$rating,
+				'review' =>$review
+			);
+			$this->api->setInsert('rating',$dataInsert);
+			$array = array('code' => 200);
+			echo json_encode($array);
+		}
 	}
 }
  ?>
